0a13b108196bd7508ae3d5b914f2d59285c462ad
MJDeveloping <63269417+MJDeveloping@users.noreply.github.com>
2020-08-20 01:15:15 +0100

    Today I mostly worked on a template that would allow me to test stuff without having to install bootstrap for every test project. Done some minor changes, like fixing the apperance of some links in the sidebar and drop down menu

---
+++ Controllers/ManageController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IssueTracker.MVC.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace IssueTracker.MVC.Controllers
+{
+    public class ManageController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly ILogger<ManageController> _logger;
+
+        public ManageController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, ILogger<ManageController> logger)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            ViewData["Title"] = "Profile";
+            ApplicationUser user = await _userManager.GetUserAsync(User);
+            return View(user);
+        }
+    }
+}
